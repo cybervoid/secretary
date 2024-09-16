6f64b077ef30a94cf2bb7ba42f6879089218cf1a
Rafael Gil <cybervoid@dumbhome.uk>
2024-09-16 19:34:03 -0400

    added .env

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module secretary
 
 go 1.23.1
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	fmt.Println("Hello, World!")
+
+	// Load the .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Get sensitive info from environment variables
+	dbUser := os.Getenv("NOTION_TOKEN")
+
+	fmt.Printf("Database user: %s, password: %s\n", dbUser, "*****")
 }
